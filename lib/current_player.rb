c302c64d2d07cd6652614cb2c1de3d5b90eb366c
Jessica Dior <jessicaclairdior@gmail.com>
2020-08-13 23:37:11 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,10 +2,10 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def turn_count(board)
 counter = 0
-board.each do |space|
+board.each {|space|
 if space == "X" || space == "O"
     counter += 1
-  end
+  end}
 end
 
 def current_player(board)
