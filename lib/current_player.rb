db7088a83cbd75d80ea829322733902d6572c166
Jessica Dior <jessicaclairdior@gmail.com>
2020-08-13 23:44:38 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,15 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
+def turn_count(board)
+counter = 0
+board.each {|space|
+if space == "X" || space == "O"
+    counter += 1
+  end}
+  counter
+end
+
+
 def current_player(board)
   turn_count(board)
   if turn_count(board) == turn_count(board) % 2
@@ -9,10 +19,3 @@ def current_player(board)
   end
 end
 
-def turn_count(board)
-counter = 0
-board.each {|space|
-if space == "X" || space == "O"
-    counter += 1
-  end}
-end
