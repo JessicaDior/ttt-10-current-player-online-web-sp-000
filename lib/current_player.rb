8438f9d5d04861f78db3ad5007bbc52168eb360a
Jessica Dior <jessicaclairdior@gmail.com>
2020-08-13 23:46:38 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -11,10 +11,6 @@ end
 
 
 def current_player(board)
-  turn_count(board)
-  if turn_count(board) == turn_count(board) % 2
-    then "O"
-  else
-    "X"
+  turn_count(board) % 2 == 0? "X" : "O"
   end
 end
